16a5177abb78ab496ff9521334f4d169eb15138e
bargest <DIZz92@mail.ru>
2024-03-19 07:15:38 +0300

    add SerMethodField

--- classes/serializers/course.py
+++ classes/serializers/course.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
 
-from classes.models import Course
+from classes.models import Course, Lesson
 
 
 class CourseSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+
+class CourseDetailSerializers(serializers.ModelSerializer):
+    count_lessons = serializers.SerializerMethodField()
+
+    def get_count_lessons(self, course):
+        return Lesson.objects.filter(course=course).count()
+
     class Meta:
         model = Course
         fields = '__all__'
