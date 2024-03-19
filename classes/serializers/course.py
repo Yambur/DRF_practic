fa1d09523dd56400c00af79bfaeabb4417dd561b
bargest <DIZz92@mail.ru>
2024-03-19 22:57:41 +0300

    add 3

--- classes/serializers/course.py
+++ classes/serializers/course.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from classes.models import Course, Lesson
+from classes.serializers.lesson import LessonSerializers
 
 
 class CourseSerializers(serializers.ModelSerializer):
@@ -11,6 +12,7 @@ class CourseSerializers(serializers.ModelSerializer):
 
 class CourseDetailSerializers(serializers.ModelSerializer):
     count_lessons = serializers.SerializerMethodField()
+    lessons = LessonSerializers(source='lesson_set', many=True)
 
     def get_count_lessons(self, course):
         return Lesson.objects.filter(course=course).count()
--- classes/serializers/lesson.py
+++ classes/serializers/lesson.py
@@ -1,9 +1,26 @@
 from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
 
-from classes.models import Lesson
+from classes.models import Lesson, Course
 
 
 class LessonSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+
+class LessonListSerializer(serializers.ModelSerializer):
+    course = SlugRelatedField(slug_field='title', queryset=Course.objects.all())
+
+    class Meta:
+        model = Lesson
+        fields = ('title', 'course',)
+
+
+class LessonDetailSerializer(serializers.ModelSerializer):
+    course = SlugRelatedField(slug_field='title', queryset=Course.objects.all())
+
     class Meta:
         model = Lesson
         fields = '__all__'
