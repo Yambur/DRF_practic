25a9dcbd6e544b07781c6e455f77804941870234
bargest <DIZz92@mail.ru>
2024-03-19 08:11:23 +0300

    add model Payment

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from classes.models import Course, Lesson
+
 NULLABLE = {'blank': True, 'null': True}
 
 
@@ -13,3 +15,23 @@ class User(AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+
+class Payment(models.Model):
+
+    PAYMENT_METHOD_CHOICES = [
+        ('CASH', "Наличные"),
+        ('BANK', "Перевод")
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Пользователь")
+    date_payment = models.DateField(verbose_name="Дата оплаты", **NULLABLE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name="Оплаченный курс", **NULLABLE)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, verbose_name="Оплаченный урок", **NULLABLE)
+    amount = models.IntegerField(max_length=15, verbose_name="Сумма оплаты", **NULLABLE)
+    payment_method = models.CharField(max_length=20, choices=PAYMENT_METHOD_CHOICES, verbose_name="Метод оплаты",
+                                      **NULLABLE)
+
+class Meta:
+    verbose_name = "Платеж"
+    verbose_name_plural = "Платежи"
