e8f4eec689e9afe924a5d6312733034a05c7d618
bargest <DIZz92@mail.ru>
2024-03-27 23:15:39 +0300

    add CRUD and urls

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
-from users.views import PaymentListView, UserUpdateView, UserDetailView
+from users.views import PaymentListView, UserUpdateView, UserDetailView, UserListView, UserCreateView, UserDeleteView
 
 app_name = UsersConfig.name
 
@@ -11,9 +12,19 @@ urlpatterns = [
     path('user/<int:pk>/', UserDetailView.as_view(), name='user_detail'),
     path('payment/', PaymentListView.as_view(), name='payment_list'),
 
+    path('user/', UserListView.as_view(), name='user_list'),
+    path('user/create/', UserCreateView.as_view(), name='user_create'),
+    path('user/<int:pk>/', UserDetailView.as_view(), name='user_detail'),
+    path('user/<int:pk>/update/', UserUpdateView.as_view(), name='user_update'),
+    path('user/<int:pk>/delete/', UserDeleteView.as_view(), name='user_delete'),
+
+    path('payment/', PaymentListView.as_view(), name='payment_list'),
+
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
-"""router = routers.SimpleRouter()
-router.register('users', UserDetailView)
+#router = routers.SimpleRouter()
+#router.register('users', UserDetailView)
 
-urlpatterns += router.urls"""
+#urlpatterns += router.urls
