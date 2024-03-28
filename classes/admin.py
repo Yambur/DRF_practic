8a9e1f8c57ce8e1a87081ba199c3a53323345aee
bargest <DIZz92@mail.ru>
2024-03-28 06:47:51 +0300

    add all. 2

--- classes/admin.py
+++ classes/admin.py
@@ -4,9 +4,9 @@ from classes.models import Course, Lesson
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'name', 'description', 'owner')
+    list_display = ('pk', 'title', 'description', 'owner')
 
 
 @admin.register(Lesson)
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'name', 'description', 'owner')
+    list_display = ('pk', 'title', 'description', 'owner')
